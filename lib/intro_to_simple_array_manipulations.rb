55e78c9b0cd890dc972f09f98f2d45c96703da38
Alexander Frosell <sachafrosell@gmail.com>
2018-10-03 22:40:12 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,12 @@ def using_push(array, string)
   array.push(string)
 end
 
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  pop_element = array.pop()
+end
+
 def 
